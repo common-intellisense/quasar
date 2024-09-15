de5cb7f60c0ece54567d7840c7694da2f2685966
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-09-15 15:17:13 +0800

    chore: init

---
+++ src/quasar2/index.ts
@@ -0,0 +1,20 @@
+import { getComponentMap, getPropsMap } from './mapping'
+
+export function quasar2() {
+  return {
+    uiName: 'quasar2',
+    map: getPropsMap(),
+    lib: 'quasar',
+    prefix: 'q',
+  }
+}
+
+export function quasar2Components() {
+  return {
+    map: getComponentMap(),
+    isSeperatorByHyphen: true,
+    prefix: 'q',
+    lib: 'quasar',
+    isReact: false,
+  }
+}
